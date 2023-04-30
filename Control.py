cce2d146bbbc76082fe9791f03090d2abce34057
Mariluz Loaiza Sanchez <mloaiza@itcol.com>
2023-04-29 22:14:59 -0500

    version3

--- Control.py
+++ Control.py
@@ -1,3 +1,14 @@
-class Control:
-    print ("MENU")
-    print("New code")
+class Vista:
+    print ("MENU DE CALCULAR VALOR H.E, R.N, H.E.D, H.E.D.N")
+    print("1.VALOR HORA EXTRADIURNA")
+    print("2.VALOR RECARGO NOCTURNO")
+    print("3.VALOR HORA EXTRA NOCTURNA")
+    print("4.VALOR HORA EXTRA DOMINICAL D")
+    print("5.VALOR HORA EXTRA DOMINICAL N")
+
+    option=input()
+
+    print("INGRESE SUELDO BASE")
+    SB=input()
+    print("INGRESE TOTAL DE HORAS EXTRAS")
+    HE=input()
